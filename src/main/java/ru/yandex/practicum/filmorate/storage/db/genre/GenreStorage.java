e52106ea70043a7d4b4c758a4961facfcf331312
Ekaterina Bogacheva <bogacheva.e@gmail.com>
2023-04-19 10:43:57 +0300

    feat: FilmGenreDbStorageTest, FilmMpaDbStorageTest, LikeDbStorageTest

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/genre/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/genre/GenreStorage.java
@@ -3,8 +3,6 @@ package ru.yandex.practicum.filmorate.storage.db.genre;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 public interface GenreStorage {
     Genre getGenreById(int genreId);

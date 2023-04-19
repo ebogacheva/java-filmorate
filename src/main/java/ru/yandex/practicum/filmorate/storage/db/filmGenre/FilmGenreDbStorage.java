88cd60050e40323f82b3f7e2146ece4d50f9ca7a
Ekaterina Bogacheva <bogacheva.e@gmail.com>
2023-04-19 10:43:57 +0300

    feat: FilmGenreDbStorageTest, FilmMpaDbStorageTest, LikeDbStorageTest

--- src/main/java/ru/yandex/practicum/filmorate/storage/db/filmGenre/FilmGenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/db/filmGenre/FilmGenreDbStorage.java
@@ -16,9 +16,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FilmGenreDbStorage implements FilmGenreStorage {
 
-    private final static String SQL_QUERY_ADD_GENRE_FOR_FILM = "INSERT INTO film_genre (film_id, genre_id) values(?, ?)";
-    private final static String SQL_QUERY_DELETE_FILM_GENRE = "DELETE FROM film_genre WHERE film_id = ? and genre_id = ?";
-    private final static String SQL_QUERY_GET_ALL_FILM_GENRES =
+    private static final String SQL_QUERY_ADD_GENRE_FOR_FILM = "INSERT INTO film_genre (film_id, genre_id) values(?, ?)";
+    private static final String SQL_QUERY_DELETE_FILM_GENRE = "DELETE FROM film_genre WHERE film_id = ? and genre_id = ?";
+    private static final String SQL_QUERY_GET_ALL_FILM_GENRES =
             "SELECT g.id, g.name FROM genre AS g " +
                     "LEFT JOIN film_genre AS fg ON g.id = fg.genre_id WHERE fg.film_id = ?";
 
